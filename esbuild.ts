b52e5ca946e3d1ae13c1d28ae208c525d0899daf
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2024-09-18 21:52:49 -0300

    Update .vscode/settings.json and package.json

--- esbuild.ts
+++ esbuild.ts
@@ -1,8 +1,25 @@
 import * as esbuild from 'esbuild';
 
-await esbuild.build({
+const args = process.argv.slice(2);
+const watch = args[0] === 'watch';
+
+//
+//
+
+const config: esbuild.BuildOptions = {
   entryPoints: ['./src/index.ts'],
   bundle: true,
   outdir: 'dist',
   format: 'esm',
-});
+};
+
+//
+//
+
+if (watch) {
+  const context = await esbuild.context(config);
+  await context.watch();
+  console.log('Watching...');
+} else {
+  await esbuild.build(config);
+}
